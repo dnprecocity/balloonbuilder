975395a9852ada631d8b95b7c85422034d20ddd5
darryl <nguyen.darryl@gmail.com>
2021-03-09 12:45:45 -0800

    Updates

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1106210.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 42165655, 42165656, 42165657, 42165659, 42165660, 42165658, 42165662, 42165663, 42165664, 42165666, 42165669, 42298246, 42546740, 42937516, 43132725, 43134141, 43599444, 43822170 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/42165566/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/42165567/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/42165565/1/ammo.js', 'preload' : true},
+];
